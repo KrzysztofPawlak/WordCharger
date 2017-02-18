1d7a17151495167744ebb391bdfd8a1b06eb07fe
kpawlak@wi.zut.edu.pl <kpaaaw@gmail.com>
2017-02-18 20:38:18 +0100

    Initial commit

---
+++ android/src/com/krzych/wordcharger/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.krzych.wordcharger;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.krzych.wordcharger.WordCharger;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WordCharger(), config);
+	}
+}
---
+++ desktop/src/com/krzych/wordcharger/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.krzych.wordcharger.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.krzych.wordcharger.WordCharger;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WordCharger(), config);
+	}
+}
