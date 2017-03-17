5feac06f532dddca6d4cddc65dfeb698181f3dff
Krzysztof Pawlak <kpawlak@wi.zut.edu.pl>
2017-03-17 23:29:29 +0100

    added viewport, camera and hud

---
+++ core/src/com/krzych/wordcharger/scenes/Hud.java
@@ -0,0 +1,45 @@
+package com.krzych.wordcharger.scenes;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/**
+ * Created by Krzysiek on 2017-03-17.
+ */
+
+public class Hud {
+
+    public Stage stage;
+    private Viewport viewport;
+
+    private Integer score;
+
+    Label scoreLabel;
+    Label levelLabel;
+
+    public Hud(SpriteBatch sb) {
+        score = 0;
+
+        viewport = new FitViewport(800, 480, new OrthographicCamera());
+        stage = new Stage(viewport, sb);
+
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
+
+        scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        levelLabel = new Label(String.format("1", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+        table.add(scoreLabel).expandX().padTop(10);
+        table.add(levelLabel).expandX().padTop(10);
+
+        stage.addActor(table);
+    }
+}
--- core/src/com/krzych/wordcharger/screens/PlayScreen.java
+++ core/src/com/krzych/wordcharger/screens/PlayScreen.java
@@ -1,11 +1,14 @@
 package com.krzych.wordcharger.screens;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.krzych.wordcharger.WordCharger;
+import com.krzych.wordcharger.scenes.Hud;
 
 /**
  * Created by Krzysiek on 2017-03-14.
@@ -15,10 +18,17 @@ public class PlayScreen implements Screen {
 
     private WordCharger game;
     Texture img;
+    private OrthographicCamera camera;
+    private Viewport view;
+    private Hud hud;
 
     public PlayScreen(WordCharger game) {
         this.game = game;
         img = new Texture("badlogic.jpg");
+        camera = new OrthographicCamera();
+        view = new StretchViewport(800, 480, camera); // TODO more universal
+
+        hud = new Hud(game.batch);
     }
 
     @Override
@@ -30,14 +40,14 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
-        game.batch.draw(img, 0, 0);
-        game.batch.end();
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        view.update(width, height);
     }
 
     @Override
