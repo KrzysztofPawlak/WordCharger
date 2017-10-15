3849467eb00476696080d7b19e9f91bb81bed83d
krzysiek_pawlak <kpaaaw@gmail.com>
2017-10-15 02:29:17 +0200

    display sentence

--- core/src/com/krzych/wordcharger/scenes/Hud.java
+++ core/src/com/krzych/wordcharger/scenes/Hud.java
@@ -1,9 +1,12 @@
 package com.krzych.wordcharger.scenes;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -25,6 +28,7 @@ public class Hud implements Disposable {
 
     Label scoreLabel;
     Label levelLabel;
+    Label sentenceLabel;
 
     public Hud(SpriteBatch sb) {
         score = 0;
@@ -39,10 +43,27 @@ public class Hud implements Disposable {
         scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
         levelLabel = new Label(String.format("1", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 
+        tryDrawChinese("我不能再等了", 64);
+
         table.add(scoreLabel).expandX().padTop(10);
         table.add(levelLabel).expandX().padTop(10);
+        table.add(sentenceLabel).expandX().padTop(10);
 
         stage.addActor(table);
+    }
+
+    private void tryDrawChinese(String sentence, int size) {
+        FreeTypeFontGenerator gen = new FreeTypeFontGenerator(Gdx.files.internal("DroidSansFallbackFull.ttf"));
+
+        FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+        parameter.size = size;
+        parameter.characters = sentence;
+
+        BitmapFont font = gen.generateFont(parameter); // font size in pixels
+
+        gen.dispose();
+
+        sentenceLabel = new Label(parameter.characters, new Label.LabelStyle(font, Color.RED));
     }
 
     @Override
